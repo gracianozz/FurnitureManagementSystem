9481c0a57b2d34acc754742eb17e8a289a4aceb2
JallenOnGit <85534745+JallenOnGit@users.noreply.github.com>
2026-04-25 21:57:33 -0500

    Update inventory_class.py
    Added variable price

--- inventory_class.py
+++ inventory_class.py
@@ -1,12 +1,14 @@
 
 class Inventory():
-    def __init__(self, inventoryID, name, skuID, location, quantity, status):
+    
+    def __init__(self, inventoryID, name, skuID, location, quantity, status, price):
         self.inventoryID = inventoryID
         self.name = name
-        self.skuID = skuID
+        self.skuID = skuID 
         self.location = location
         self.quantity = quantity
         self.status = status
+        self.price = price
 
 
 
